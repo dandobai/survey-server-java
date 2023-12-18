f82f7ce047317115f9c016c66399a83d7d5b23e0
dandobai <dandobai@gmail.com>
2023-12-18 18:05:32 +0100

    gitignore update

---
+++ src/main/java/com/surver/SurverApplication.java
@@ -0,0 +1,13 @@
+package com.surver.surver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/surver/surver/SurverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.surver.surver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
